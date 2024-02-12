71ae79a894ffb0e04910da6bd019541019d3fbd6
kaostnery <thibault.roux2002@gmail.com>
2024-02-12 21:36:13 +0100

    opdazp,a,dae

--- tp1/lib/src/widgets/media_list.dart
+++ tp1/lib/src/widgets/media_list.dart
@@ -3,6 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../data.dart';
 
@@ -10,11 +11,15 @@ class MediaList extends StatelessWidget {
   final List<Media> medias;
   final ValueChanged<Media>? onTap;
 
+
   const MediaList({
     required this.medias,
     this.onTap,
+
     super.key,
   });
+
+
 
   @override
   Widget build(BuildContext context) => ListView.builder(
@@ -27,6 +32,18 @@ class MediaList extends StatelessWidget {
             medias[index].author.name,
           ),
           onTap: onTap != null ? () => onTap!(medias[index]) : null,
+
+          trailing: IconButton(
+            key: Key(medias[index].title),
+            icon: medias[index].isFavoris==true 
+              ? const Icon(Icons.favorite)
+              : const Icon(Icons.favorite_border),
+            onPressed :(){
+             medias[index].isFavoris==true
+            ? medias[index].isFavoris==false
+            : medias[index].isFavoris==true; 
+            }),
+
         ),
       );
 }
